ff183aa46b0bfe96fd0472b47c9c816fb92ea12d
KyoungminLeeWego <km.lee@wego-robotics.com>
2026-04-09 16:08:24 +0900

    라이다 장애물 인식 추가

--- vizanti_server/public/widgets/patrol_route/patrolroute_script.js
+++ vizanti_server/public/widgets/patrol_route/patrolroute_script.js
@@ -274,6 +274,7 @@ function {uniqueID}_renderList() {
 			{ value: '',            label: '없음' },
 			{ value: 'yolo_camera', label: '📷 카메라' },
 			{ value: 'photo',       label: '🖼 사진 저장' },
+			{ value: 'lidar_check', label: '📡 전방 장애물 감지' },
 			{ value: 'alarm',       label: '🚨 경보' },
 		];
 		const currentOption = (wp.patrol_options && wp.patrol_options[0]) || '';
